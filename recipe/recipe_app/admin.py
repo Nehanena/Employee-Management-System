652c9af68eca88b44d9e06b1a61e5116df696afe
Nehanena <nehanaithik12@gmail.com>
2025-09-22 19:00:11 +0530

    Delete recipe directory

--- recipe/recipe_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Recipe, Ingredient, Instruction
-
-# Unregister if already registered
-admin.site.register(Ingredient)
-admin.site.register(Instruction)
-admin.site.register(Recipe)
-
