890c05d82ca672fec22bb713e4f1e4d06bcac95b
Nehanena <nehanaithik12@gmail.com>
2025-09-22 19:01:19 +0530

    Delete academy directory

--- academy/academy_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from.models import Reg_tbl
-# Register your models here.
-admin.site.register (Reg_tbl)
--- academy/academy_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Reg_tbl(models.Model):
-    name = models.CharField(max_length=25)
-    number = models.IntegerField()
-    email = models.EmailField()
-    passw = models.CharField(max_length=25)
